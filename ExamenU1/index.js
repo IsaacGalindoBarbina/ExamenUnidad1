8172a63b8d5bd479a4e322e3c4f2286bfc19b42a
IsaacGalindo <84301741+IsaacGalindoBarbina@users.noreply.github.com>
2023-06-06 20:13:45 -0600

    Isaac Jacobo Galindo Barbina

---
+++ ExamenU1/index.js
@@ -0,0 +1,42 @@
+const televisor = {
+    "power" : false,
+    "subirVolumen" : 0,
+    "bajarVolumen" : 0,
+    "subirCanal": 0,
+    "bajarCanal": 0,
+    "activarDesactivar": false
+}
+
+function power(){
+    if (televisor.power) {
+        televisor.power = false;
+        console.log("tele encendida");
+    } else {
+        televisor.power = true;
+        console.log("tele apagada");
+    }
+}
+
+function subirVolumen(){
+    if (televisor.subirVolumen ) {
+        
+    } else {
+        
+    }
+}
+
+function bajarVolumen(){
+
+}
+
+function subirCanal(){
+
+}
+
+function bajarCanarl(){
+
+}
+
+function activarDesactivar(){
+
+}
